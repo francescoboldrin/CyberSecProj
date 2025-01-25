ee453798299b2709ab7b467ed3cbd0a1412cfeec
marius_the_epicurean <95216276+francescoboldrin@users.noreply.github.com>
2025-01-25 15:04:32 +0100

    Update MailSender.py

--- mail_sender/MailSender.py
+++ mail_sender/MailSender.py
@@ -9,7 +9,7 @@ from MailCrafter import craft_mail
 def send_mail(receiver):
     # Email details
     receiver_email = receiver["email"]  # The receiver's email address
-    sender_email = "cybersecproj1@zohomail.eu"  # Your authenticated Zoho email address
+    sender_email = "yourmail@mail"  # Your authenticated Zoho email address
     subject = "Login credentials retrieval"  # The subject of the email
     body = craft_mail(receiver)  # The body of the email
 
@@ -27,7 +27,7 @@ def send_mail(receiver):
 
     # Zoho SMTP credentials
     smtp_user = sender_email
-    smtp_password = "Merlino.00"  # Use an app-specific password if 2FA is enabled
+    smtp_password = "yourpassword"  # Use an app-specific password if 2FA is enabled
 
     # Establish connection to the SMTP server
     try:
